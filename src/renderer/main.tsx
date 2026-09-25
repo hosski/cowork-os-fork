cb5c83af05ac1b857639fa855ae83c0d502acbe8
Russell Skow <hosski@Russells-MacBook-Pro-2.local>
2026-09-24 21:26:05 -0400

    fix: restore correct react-refresh import path
    - Changed back to './react-refresh-ignored-exports' (local file)
    - 'react-refresh/ignored-exports' doesn't exist in node_modules

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "react-refresh/ignored-exports";
+import "./react-refresh-ignored-exports";
 import { App } from "./App";
 import { CoWorkReduxWrapper } from "./redux-provider";
 import "./styles/index.css";
