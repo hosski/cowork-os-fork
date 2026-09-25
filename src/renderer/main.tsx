0fc44a5868969a364202b9657b92dcea691bb44c
Russell Skow <hosski@Russells-MacBook-Pro-2.local>
2026-09-24 21:25:02 -0400

    fix: wrap app with Redux Provider in main.tsx
    - Added CoWorkReduxWrapper from redux-provider.tsx
    - GrillTabPanel and TaskDAGViewer now have Redux context
    - Fixed 'could not find react-redux context value' error

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./react-refresh-ignored-exports";
+import "react-refresh/ignored-exports";
 import { App } from "./App";
+import { CoWorkReduxWrapper } from "./redux-provider";
 import "./styles/index.css";
 import "./components/right-panel.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <CoWorkReduxWrapper>
+      <App />
+    </CoWorkReduxWrapper>
   </React.StrictMode>,
 );
